b5cceaa36b23fb2a6a61426755fecd9552b0eee4
John Tracy <johnmtracy133@hotmail.com>
2016-09-07 12:40:05 -0400

    client side login logic

--- app/public/javascript/login.js
+++ app/public/javascript/login.js
@@ -1,1 +1,58 @@
-$('#signup-modal').modal('toggle');
+// on-click for sign-up modal
+$('#new-user').on('click', function(){
+	$('#signup-modal').modal('toggle');
+	return false;
+});
+
+// on-click for sending new user info
+$('#input-newUser').on('click', function(){
+	var password = $('#password-signup').val().trim()
+	var passwordValid = $('#signup-password-auth').val().trim()
+	console.log(password);
+	console.log(passwordValid);
+	// makes certain that they are using the password they want to use.
+	if(password === passwordValid && password != ''){
+
+		console.log('password validation worked');
+		// var firstName
+		// var lastName
+		// var userName
+		// var password
+		return false;
+	}
+	else{
+		console.log('passwords didnt match');
+		return false;
+	}
+});
+
+//on-click for signing in
+$('#user-signin').on('click', function(){
+	var userName = $('#inputUserName').val().trim();
+	var password = $('#inputPassword').val().trim();
+
+	$('#inputUserName').val('');
+	$('#inputPassword').val('');
+
+	if(userName == "admin"){
+		console.log('admin ajax');
+	}
+	else {
+		console.log('user ajax');
+	}
+	
+	return false;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
